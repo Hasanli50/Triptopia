e95865f6d4db159c2e4dd491f1a4aa481abede5a
Ayla <ayla.hasanli50@gmail.com>
2025-04-24 15:33:53 +0400

    fix mui grid deprecated  tag

--- client/src/pages/user/Home.tsx
+++ client/src/pages/user/Home.tsx
@@ -1,28 +1,32 @@
-import { Grid } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import firstSectionImage from "../../assets/photo/first section - home page.png";
 import style from "../../assets/style/user/home.module.scss";
 
 const Home = () => {
   return (
-    <>
-      <section className={style.home}>
-        <Grid container spacing={8}>
-          <Grid size={8}>
-            <div className={style.firstColumn}>
-              <p className={style.travellingWithUs}>Start Travelling with us</p>
-              <h3 className={style.enjoyWithTourice}>Let’s enjoy your desired trip with Tourice</h3>
-              <p className={style.paragraph}>
-                The traveller where you can select your desired activity and
-                destinations of your choice for vacations.
-              </p>
-            </div>
-          </Grid>
-          <Grid>
-            <img className={style.secondSectionImage} src={firstSectionImage} alt="ffirst section image" />
-          </Grid>
+    <section className={style.home}>
+      <Grid container spacing={8}>
+        <Grid item xs={12} md={8}>
+          <div className={style.firstColumn}>
+            <p className={style.travellingWithUs}>Start Travelling with us</p>
+            <h3 className={style.enjoyWithTourice}>
+              Let’s enjoy your desired trip with Tourice
+            </h3>
+            <p className={style.paragraph}>
+              The traveller where you can select your desired activity and
+              destinations of your choice for vacations.
+            </p>
+          </div>
         </Grid>
-      </section>
-    </>
+        <Grid item xs={12} md={4}>
+          <img
+            className={style.secondSectionImage}
+            src={firstSectionImage}
+            alt="first section image"
+          />
+        </Grid>
+      </Grid>
+    </section>
   );
 };
 
